7a89d5a5ec814d203b4a0660b99ae3226e869043
Litesh Samji <litesh@y7mail.com>
2021-08-10 17:11:43 +1000

    Javascript code to append the index.html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,23 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+// iterate through the data
+    data.forEach((dataRow) => {
+        //Apend a row to the table body
+        const row = tbody.append("tr");
+        //Loop through each field in the dataRow and add
+        //each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell= row.append("td")
+            cell.text(val);
+            //let row.append("td").text(val);
+            }
+        ); 
+    });
+}
+
+buildTable(tableData);
