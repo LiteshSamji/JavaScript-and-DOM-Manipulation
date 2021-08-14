84bd329dd215e5e2f8f8bb7071c3353240836b08
Litesh Samji <litesh@y7mail.com>
2021-08-14 16:58:38 +1000

    Level 1: final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,6 +4,8 @@ var tableData = data;
 // YOUR CODE HERE!
 
 var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
 
 function buildTable(data) {
 // iterate through the data
@@ -15,10 +17,32 @@ function buildTable(data) {
         Object.values(dataRow).forEach((val) => {
             let cell= row.append("td")
             cell.text(val);
-            //let row.append("td").text(val);
             }
         ); 
     });
 }
-
+//Load data
 buildTable(tableData);
+
+button.on("click", () => {
+    //Prevent the page from refreshing
+    d3.event.preventDefault();
+    //Select the Input element
+    var inputDate = inputFieldDate.property("value").trim();
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+    
+    tbody.html("");
+
+    let response = {
+        filterDate
+    }
+
+    if(response.filterDate.length !== 0) {
+        buildTable(filterDate);
+    }
+    // add comment if not sightings    
+        else {
+            tbody.append("tr").append("td").text("No Data Found ...");
+        }
+})
+
